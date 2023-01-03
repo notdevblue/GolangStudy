a194357ee7caf07cdcf9ce786f02ba473a7d0e95
HanAtNGLE <wooyeophan@gmail.com>
2023-01-03 18:58:00 +0900

    테스트 코드

--- Jan_3/helloworld.go
+++ Jan_3/helloworld.go
@@ -5,5 +5,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	const (
+		A int    = 10
+		B        = "Hello"
+		C string = "CString"
+	)
+
+	var (
+		a string  = "Hello"
+		b         = 10
+		c float32 = 1.54
+	)
+
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Println()
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	name := "Han"
+
+	const GENTOO string = "Gentoo"
+	const LINUX = "Linux"
+
+	fmt.Println("Name is " + name)
 }
